0a27ee91f5c1e0473e34932a2268015e601b04b0
big-Bong <vasuchat@gmail.com>
2024-03-16 00:46:07 +0530

    Updated channels in model_1 to reduce params

--- Session7/model_1.py
+++ Session7/model_1.py
@@ -15,20 +15,20 @@ class Net(nn.Module):
         ) # output_size = 26
 
         self.convblock2 = nn.Sequential(
-            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=(3, 3), padding=0, bias=False),
+            nn.Conv2d(in_channels=16, out_channels=20, kernel_size=(3, 3), padding=0, bias=False),
             nn.ReLU(),
-            nn.BatchNorm2d(32),
+            nn.BatchNorm2d(20),
             nn.Dropout(0.10)
         ) # output_size = 24
 
         #Adding AntMan and MaxPool
         self.convblock3 = nn.Sequential(
-            nn.Conv2d(in_channels=32, out_channels=8, kernel_size=(1, 1), padding=0, bias=False),
+            nn.Conv2d(in_channels=20, out_channels=10, kernel_size=(1, 1), padding=0, bias=False),
         ) # output_size = 24
         self.pool1 = nn.MaxPool2d(2, 2) # output_size = 12
 
         self.convblock4 = nn.Sequential(
-            nn.Conv2d(in_channels=8, out_channels=16, kernel_size=(3, 3), padding=0, bias=False),
+            nn.Conv2d(in_channels=10, out_channels=16, kernel_size=(3, 3), padding=0, bias=False),
             nn.ReLU(),
             nn.BatchNorm2d(16),
             nn.Dropout(0.10)
@@ -42,21 +42,21 @@ class Net(nn.Module):
         ) # output_size = 8
 
         self.convblock6 = nn.Sequential(
-            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=(3, 3), padding=0, bias=False),
+            nn.Conv2d(in_channels=16, out_channels=16, kernel_size=(3, 3), padding=0, bias=False),
             nn.ReLU(),
-            nn.BatchNorm2d(32),
+            nn.BatchNorm2d(16),
             nn.Dropout(0.10)
         ) # output_size = 6
 
         self.convblock7 = nn.Sequential(
-            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(3, 3), padding=1, bias=False),
+            nn.Conv2d(in_channels=16, out_channels=20, kernel_size=(3, 3), padding=1, bias=False),
             nn.ReLU(),
-            nn.BatchNorm2d(32),
+            nn.BatchNorm2d(20),
             nn.Dropout(0.10)
         ) # output_size = 6
 
         self.convblock8 = nn.Sequential(
-            nn.Conv2d(in_channels=32, out_channels=10, kernel_size=(6, 6), padding=0, bias=False),
+            nn.Conv2d(in_channels=20, out_channels=10, kernel_size=(6, 6), padding=0, bias=False),
         ) # output_size = 1
     
     def forward(self, x):
